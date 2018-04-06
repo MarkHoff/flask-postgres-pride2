0ee9ee0702017db37b37bcba82bffd4067b4783e
Mark Hoffman <mark.hoffman9@gmail.com>
2018-04-05 20:32:59 -0400

    add migrate command to Procfile to load postgres on Heroku and modified Config.py to connect to Heroku Postgres database URL

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, Re
     ResetPasswordForm, ProjectForm, EditProjectForm, DbObjectForm, EditDbObjectForm, SearchForm
 from app.email import send_password_reset_email
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User, Post, Project, DbObject
+from app.models import User, Project, DbObject
 from werkzeug.urls import url_parse
 from datetime import datetime
 
@@ -56,19 +56,19 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route('/user/<username>')
-@login_required
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    page = request.args.get('page', 1, type=int)
-    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('user', username=user.username, page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
-        if posts.has_prev else None
-    return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_url=prev_url)
+# @app.route('/user/<username>')
+# @login_required
+# def user(username):
+#     user = User.query.filter_by(username=username).first_or_404()
+#     page = request.args.get('page', 1, type=int)
+#     posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+#         page, app.config['POSTS_PER_PAGE'], False)
+#     next_url = url_for('user', username=user.username, page=posts.next_num) \
+#         if posts.has_next else None
+#     prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+#         if posts.has_prev else None
+#     return render_template('user.html', user=user, posts=posts.items,
+#                            next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -87,18 +87,18 @@ def edit_profile():
     return render_template('edit_profile.html', title='Edit Profile',
                            form=form)
 
-@app.route('/explore')
-@login_required
-def explore():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
-        page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('explore', page=posts.next_num) \
-        if posts.has_next else None
-    prev_url = url_for('explore', page=posts.prev_num) \
-        if posts.has_prev else None
-    return render_template("index.html", title='Explore', posts=posts.items,
-                          next_url=next_url, prev_url=prev_url)
+# @app.route('/explore')
+# @login_required
+# def explore():
+#     page = request.args.get('page', 1, type=int)
+#     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+#         page, app.config['POSTS_PER_PAGE'], False)
+#     next_url = url_for('explore', page=posts.next_num) \
+#         if posts.has_next else None
+#     prev_url = url_for('explore', page=posts.prev_num) \
+#         if posts.has_prev else None
+#     return render_template("index.html", title='Explore', posts=posts.items,
+#                           next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
